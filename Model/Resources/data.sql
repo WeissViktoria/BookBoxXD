2a4f3fe0c98cf5c58cc4ce2fbf7ffdef2318dafe
vik <v.weiss@htlkrems.at>
2025-03-27 15:10:02 +0100

    logik register geht noch nicht

---
+++ Model/Resources/data.sql
@@ -0,0 +1,19 @@
+-- Benutzer 1
+INSERT INTO BENUTZER (BENUTZER_NAME, PASSWORT, EMAIL_ADDRESSE, GEBURTSTAG)
+VALUES ('MaxMustermann', 'Passwort123', 'max.mustermann@example.com', '1990-05-15');
+
+-- Benutzer 2
+INSERT INTO BENUTZER (BENUTZER_NAME, PASSWORT, EMAIL_ADDRESSE, GEBURTSTAG)
+VALUES ('AnnaMusterfrau', 'SicheresPasswort1', 'anna.musterfrau@example.com', '1985-08-22');
+
+-- Benutzer 3
+INSERT INTO BENUTZER (BENUTZER_NAME, PASSWORT, EMAIL_ADDRESSE, GEBURTSTAG)
+VALUES ('JohnDoe', 'SuperSecure123', 'john.doe@example.com', '2000-01-30');
+
+-- Benutzer 4
+INSERT INTO BENUTZER (BENUTZER_NAME, PASSWORT, EMAIL_ADDRESSE, GEBURTSTAG)
+VALUES ('JuliaSchmidt', 'Passw0rd!', 'julia.schmidt@example.com', '1995-03-10');
+
+-- Benutzer 5
+INSERT INTO BENUTZER (BENUTZER_NAME, PASSWORT, EMAIL_ADDRESSE, GEBURTSTAG)
+VALUES ('StefanMüller', 'Müller@2025', 'stefan.mueller@example.com', '1988-12-04');
